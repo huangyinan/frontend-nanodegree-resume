6b5037891db47248207de9664434cfd50bd500de
Sherry Huang <Sherry Huang>
2017-05-01 20:02:34 +0800

    uewet

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -7,15 +7,22 @@ var work = {
       "employer": "SSI Schaefer",
       "title": "IT Project Engineer",
       "location": "Shanghai",
-      "dates": "from 2015/03/01 until now",
-      "description": ""
+      "dates": "2015/03/01 until now",
+      "description": "Designing and implementing as Leading project engineer for MFS system"
     },
     {
-      "employer": "SSI Schaefer",
-      "title": "IT Project Engineer",
+      "employer": "FCH",
+      "title": "IT Consultant",
       "location": "Shanghai",
-      "dates": "from 2015/03/01 until now",
-      "description": ""
+      "dates": "2012/2/1 to 2015/03/01",
+      "description": "Consultant"
+    },
+    {
+      "employer": "New touch",
+      "title": "programmer",
+      "location": "Shanghai",
+      "dates": "2010/7/1 to 2012/02/01",
+      "description": "programmer"
     }
   ]
 };
@@ -77,7 +84,54 @@ var education = {
   ]
 };
 
-education["city"] = "Shanghai, China";
+if (bio.role != null) {
+  var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
+  $("#header").prepend(formattedRole);
+}
 
-$("#main").append(work["position"]);
-$("#main").append(education.name);
+if (bio.name != null) {
+  var formattedName = HTMLheaderName.replace("%data%",bio.name);
+  $("#header").prepend(formattedName);
+}
+
+if (bio.skills.length > 0) {
+  $("#header").append(HTMLskillsStart);
+  for (skill in bio.skills) {
+    var formattedStill = HTMLskills.replace("%data%",bio.skills[skill]);
+    $("#skills").append(formattedStill);
+  }
+}
+
+
+if (work.jobs.length > 0) {
+  for (job in work.jobs) {
+    $("#workExperience").append(HTMLworkStart);
+
+    var formattedEmployer = HTMLworkEmployer.replace("%data%",work.jobs[job].employer);
+    var formattedTitle = HTMLworkTitle.replace("%data%",work.jobs[job].title);
+    var formattedEmployerTitle = formattedEmployer + formattedTitle;
+    $(".work-entry:last").append(formattedEmployerTitle);
+
+    var formattedDates = HTMLworkDates.replace("%data%",work.jobs[job].dates);
+    $(".work-entry:last").append(formattedDates);
+
+    var formattedLocation = HTMLworkDates.replace("%data%",work.jobs[job].location);
+    $(".work-entry:last").append(formattedLocation);
+
+    var formattedDescription = HTMLworkDates.replace("%data%",work.jobs[job].description);
+    $(".work-entry:last").append(formattedDescription);
+  }
+}
+
+$(document).click(function(loc) {
+  // your code goes here
+  var x = loc.pageX;
+  var y = loc.pageY;
+
+  logClicks(x,y);
+});
+
+
+
+// $("#main").append(work["position"]);
+// $("#main").append(education.name);
